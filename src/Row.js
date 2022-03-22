8d5a2405c19d22507a78c46451afb2f67230ce91
rory <rory-smith14@hotmail.co.uk>
2022-03-22 22:12:53 +0000

    more css on main page

--- src/Row.js
+++ src/Row.js
@@ -21,20 +21,54 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
 
     if(movies){
 
+        if(title == "Trending Now"){
+            return (
+                <div className="row">
+                    <h2>{title}</h2>
+            
+                    <div className="row__posters_trending">
+                        {movies.map((movie) => (
+                            // isLargeRow && movie.Poster_path ||
+                            // !isLargeRow && movie.backDrop_path && (
+                              <img 
+                                className={`row__poster`}
+                                key={movie.id}
+                                src={`${base_url}${
+                                  movie.backdrop_path
+                                }`} 
+                                alt={movie.name}
+                            />
+                        ))
+                    }
+                    </div>
+            
+                    
+                </div>
+              );
+
+        }
+
   return (
     <div className="row">
         <h2>{title}</h2>
 
-        {movies.map((movie) => (
-            <img 
-            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-            key={movie.id}
-                src={`${base_url}${
-                    isLargeRow ? movie.poster_path : movie.backdrop_path
-                }`} 
-                alt={movie.name}
-            />
-        ))}
+        <div className="row__posters">
+            {movies.map((movie) => (
+                // isLargeRow && movie.Poster_path ||
+                // !isLargeRow && movie.backDrop_path && (
+                  <img 
+                    className={`row__poster`}
+                    key={movie.id}
+                    src={`${base_url}${
+                      movie.backdrop_path
+                    }`} 
+                    alt={movie.name}
+                />
+            ))
+        }
+        </div>
+
+        
     </div>
   );
 }
